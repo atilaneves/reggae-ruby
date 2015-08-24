f8fa3c982afd1f59431b4867724fdacb4bb1feab
Atila Neves <aalvesne@cisco.com>
2015-08-24 16:58:55 +0200

    Properly failing JSON test

--- spec/to_json_spec.rb
+++ spec/to_json_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe Target, '#to_json' do
   context 'Leaf target' do
     it 'returns correct json' do
       tgt = Target.new('foo.d')
-      expect(tgt.to_json).to be_json_eql('{}')
+      expect(tgt.to_json).to be_json_eql(
+        '{"type": "fixed",
+          "command": {},
+          "outputs": ["foo.d"],
+          "dependencies": {"type": "fixed", "targets": []},
+          "implicits": {"type": "fixed", "targets": []}}')
     end
   end
 end
