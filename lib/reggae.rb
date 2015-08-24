188e37afb6f389371cb4e434ebe209573aa42f8b
Atila Neves <aalvesne@cisco.com>
2015-08-24 16:06:39 +0200

    Failing test for get_build

---
+++ lib/reggae.rb
@@ -0,0 +1,15 @@
+class BuildFinder
+  def self.get_build
+  end
+end
+
+
+class Build
+  def initialize(tgt)
+  end
+end
+
+class Target
+  def initialize(outputs, command='', dependencies=[], implicits=[])
+  end
+end
