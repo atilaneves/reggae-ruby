f2bcbd94a12b1b1284987bc75549fe20e501a283
Atila Neves <aalvesne@cisco.com>
2015-08-24 16:39:34 +0200

    Working finder for the Build object

--- lib/reggae.rb
+++ lib/reggae.rb
@@ -1,14 +1,22 @@
-class BuildFinder
+# Find the build object
+module BuildFinder
   def self.get_build
+    builds = []
+    ObjectSpace.each_object(Build) { |x| builds << x }
+    if builds.length != 1
+      fail "Only one Build object may exist, found #{builds.length}"
+    end
+    builds[0]
   end
 end
 
-
+# Aggregates top-level targets
 class Build
   def initialize(tgt)
   end
 end
 
+# A build target
 class Target
   def initialize(outputs, command='', dependencies=[], implicits=[])
   end
